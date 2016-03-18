ef326da08c372e0c470ab6177f2d360335f24fd1
codeepeng <christianpl8975@gmail.com>
2016-03-18 01:29:49 -0500

    this is how you can edit multiple files

--- hello02.java
+++ hello02.java
@@ -3,6 +3,7 @@ public class hello02 {
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, 02");
+	//change the second
     }
 
 }
