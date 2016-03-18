da146ecc4fb418662321696d506aa66d6cad7f48
codeepeng <christianpl8975@gmail.com>
2016-03-18 01:34:52 -0500

    we are commiting

--- hello.java
+++ hello.java
@@ -1,6 +1,7 @@
 public class hello{
 	public static void main(String[] args){
 		System.out.println("Hello CS580!!");
-		system.out.println("Hello CS580 again!!");
+		System.out.println("Hello CS580 again!!");
+		System.out.println("nice job peng");
 	}
 }
