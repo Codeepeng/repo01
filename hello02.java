ecbb968e4f5df9005e2c67ab6d37b5ce9831b132
codeepeng <christianpl8975@gmail.com>
2016-03-18 01:21:47 -0500

    adding the second file to the repo

---
+++ hello02.java
@@ -0,0 +1,8 @@
+public class hello02 {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, 02");
+    }
+
+}
